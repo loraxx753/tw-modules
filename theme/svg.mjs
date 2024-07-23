a689c8bbbfd8c634e9cfc96ea2f664d8ae9050d4
Kevin Baugh <loraxx753@gmail.com>
2024-07-23 17:25:59 -0500

    Filled out theme files and emptied extentions.

--- theme/svg.mjs
+++ theme/svg.mjs
@@ -1,1 +1,16 @@
-export default {}
+export default {
+    fill: ({ theme }) => ({
+        none: 'none',
+        ...theme('colors'),
+    }),
+    stroke: ({ theme }) => ({
+        none: 'none',
+        ...theme('colors'),
+    }),
+    strokeWidth: {
+        0: '0',
+        1: '1',
+        2: '2',
+    },
+    
+}
